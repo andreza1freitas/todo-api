89b3b9f0f0a20fa6624fb09c3ceffcbc96a00fe6
Andreza Freitas <andreza1freitas@gmail.com>
2022-09-02 20:02:20 -0300

    correcao do path

--- src/main/java/com/andrezafreitas/todo/rest/TodoController.java
+++ src/main/java/com/andrezafreitas/todo/rest/TodoController.java
@@ -3,6 +3,8 @@ package com.andrezafreitas.todo.rest;
 import java.time.LocalDateTime;
 import java.util.List;
 
+import javax.transaction.Transactional;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.CrossOrigin;
@@ -13,6 +15,7 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.server.ResponseStatusException;
 
@@ -42,7 +45,9 @@ public class TodoController {
 		return repository.findById(id).orElseThrow(() -> new ResponseStatusException(HttpStatus.NOT_FOUND));
 	}
 	
-	@DeleteMapping({"id"})
+	@ResponseBody
+	@DeleteMapping({"{id}"})
+	@Transactional
 	public void delete(@PathVariable Long id) {
 		repository.deleteById(id);
 	}
